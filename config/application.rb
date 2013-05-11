c125ea02297837bec66fc716a0a5ef2740aa2403
Chris Westra <cwestra@gmail.com>
2013-05-11 17:54:33 -0400

    Add devise

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module CsvLogger
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
@@ -67,5 +67,8 @@ module CsvLogger
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # for heroku, don't access the DB or load models when precompiling.
+    config.assets.initialize_on_precompile = false
   end
 end
