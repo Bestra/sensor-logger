015682eb66d8463b7e3720a2b49c855bc6cdc166
Chris Westra <cwestra@gmail.com>
2013-05-11 13:06:51 -0400

    Make a new rails 3.2.12 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CsvLogger::Application.config.secret_token = '65db067e51875ff6ff3958bbc4bd9fff43dab76e5e123c333e874e348b4936cc77c6dd69cd8e80df819884a3ad325198f71dbea6eceefa6be04795b04b5f671a'
