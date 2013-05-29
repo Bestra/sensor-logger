91f29442093be3e69b446d1471dedfdd0721653a
Chris Westra <cwestra@gmail.com>
2013-05-29 10:02:50 -0400

    More fleshing out

--- app/models/sensor_reading_query.rb
+++ app/models/sensor_reading_query.rb
@@ -7,11 +7,37 @@ class SensorReadingQuery
   validates :result_type, inclusion: {in: %i(json)}
   def initialize(project, options)
     self.project = project
-    self.sensors = project.get_sensors options[:sensor_indices]
+    self.sensors = options[:sensor_indices] ? project.get_sensors options[:sensor_indices] : project.sensors
     self.start_time = options[:start_time] ? DateTime.parse(options[:start_time]).utc : nil
     self.end_time = options [:end_time] ? DateTime.parse(options[:end_time]).utc : nil
-    self.result_limit = options[:result_limit].try(:to_i)
+    self.result_limit = options[:result_limit].try(:to_i) || 0
     self.result_type = options[:result_type].try(:to_sym) || :json
     super
+  end
+
+  def result
+    @result ||= run_query
+  end
+
+  private
+  def start_time_query(query)
+    self.start_time ? query.gte(time: self.start_time) : query
+  end
+
+  def end_time_query(query)
+    self.end_time ? query.lte(time: self.end_time) : query
+  end
+
+  def time_query(query)
+    query = start_time_query(query)
+    query = end_time_query(query)
+  end
+
+  def run_query
+    sensor_results = {}
+    self.sensors.each do |sensor|
+      sensor_results[sensor.project_index.to_s] = SensorReading.where(sensor_id: sensor).time_query.order_by(time: "asc").limit(self.result_limit)
+    end
+    sensor_results
   end
 end
--- spec/models/sensor_reading_query_spec.rb
+++ spec/models/sensor_reading_query_spec.rb
@@ -4,4 +4,18 @@ describe SensorReadingQuery do
 
 
   end
+
+
+  describe "getting results" do
+    context "with a valid project" do
+      Given(:project) { FactoryGirl.create(:project) }
+      context "with existing sensors and data" do
+      Given!(:sensors) do
+        project.find_or_create_sensor(1)
+        project.find_or_create_sensor(2)
+      end
+
+      end
+    end
+  end
 end
