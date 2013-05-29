9c6a6af24e9e36913899ce824897824a53ea7044
Chris Westra <cwestra@gmail.com>
2013-05-29 07:15:12 -0400

    Add spec for existing sensors to SensorReadingsController spec

--- spec/controllers/sensor_readings_controller_spec.rb
+++ spec/controllers/sensor_readings_controller_spec.rb
@@ -17,35 +17,47 @@ describe SensorReadingsController do
 
 
   describe '#create' do
+    Given(:reading_time) { DateTime.now.utc }
     context "with a signed in user" do
       Given(:user) { create_signed_in_user }
       context "with an existing project" do
         Given(:project) { FactoryGirl.create(:project, user_id: user.id) }
+        Given(:posted_index) { 1 }
+        Given(:two_readings) { [{time: reading_time - 1.minute, value: 3}, {time: reading_time, value: 1}] }
+        Given(:sensors_posted) { [{project_index: posted_index, readings: two_readings}] }
+        Given(:new_request) { { project_id: project.id, sensors: sensors_posted } }
         context "a reading for a new sensor" do
-          Given(:reading_time) { DateTime.now.utc }
-          Given(:sensors) { [{project_index: 1,
-                              readings: [{time: reading_time, value: 1}]
-                             }
-                            ]
-                          }
-          Given(:new_request) do
-            json = {
-              project_id: project.id,
-              sensors: sensors
-            }
-          end
-
           When { post :create, sensorReadings: new_request }
 
           Then do
-            Project.last.sensors.where(project_index: 1).should_not be_nil
+            project.reload.sensors.where(project_index: 1).should_not be_nil
           end
 
           Then do
+            SensorReading.count.should == 2
             s = SensorReading.last
             s.should_not be_nil
             s.time.to_s.should == reading_time.to_s
             s.value.should == 1
+          end
+        end
+
+        context "a reading for an existing sensor" do
+          Given(:existing_sensor) { s = project.sensors.build(project_index: 2, name: "door"); project.save; s}
+          Given(:posted_index) { existing_sensor.project_index }
+          When { post :create, sensorReadings: new_request }
+          Then do
+            binding.pry
+            Sensor.count.should == 1
+            project.reload.sensors.where(project_index: existing_sensor.project_index).name.should == existing_sensor.name
+          end
+          Then do
+            SensorReading.count.should == 2
+            s = SensorReading.last
+            s.should_not be_nil
+            s.time.to_s.should == reading_time.to_s
+            s.value.should == 1
+            s.sensor_id.should == existing_sensor.id
           end
         end
       end
