519877fcea05c936c7c7007b1bf7ef92a916879c
Chris Westra <cwestra@gmail.com>
2013-05-28 23:43:54 -0400

    Add #find_or_create to Sensor.
    Also add type to project_index field

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -4,5 +4,15 @@ class Sensor
   has_many :sensor_readings
 
   field :name
-  field :project_index
+  field :project_index, type: Integer
+
+  def self.find_or_create(project, index)
+    s = Sensor.where(project_id: project.id, project_index: index).first
+    unless s
+      s = project.sensors.build(project_index: index)
+      project.save
+    end
+    s
+  end
+
 end
