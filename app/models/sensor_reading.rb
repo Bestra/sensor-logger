5198f2a90d1ebd71d6a4c6402469a2354019660d
Chris Westra <cwestra@gmail.com>
2013-05-29 07:26:42 -0400

    Fix SensorReading#add_readings
    Properly assign a sensor_id

--- app/models/sensor_reading.rb
+++ app/models/sensor_reading.rb
@@ -7,7 +7,7 @@ class SensorReading
 
   def self.add_readings(sensor_id, readings)
     readings.reduce 0 do |added_count, reading|
-      r = SensorReading.new time: Time.parse(reading['time']), value: reading['value'].to_i
+      r = SensorReading.new sensor_id: sensor_id, time: Time.parse(reading['time']), value: reading['value'].to_i
       if r.save
         added_count + 1
       end
