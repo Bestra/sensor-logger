9d8fa22c7ed08cbda14af1147c72080a17901c0e
Chris Westra <cwestra@gmail.com>
2013-05-29 07:26:07 -0400

    Create Project#sensor()
    Finds a project's sensor by its project_index

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,10 @@ class Project
   field :name
   field :description
 
+  def sensor(index)
+    s = self.sensors.where(project_index: index).first
+  end
+
   def find_or_create_sensor(index)
     s = self.sensors.find_or_create_by(project_index: index)
   end
