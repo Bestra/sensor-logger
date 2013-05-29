6668990d6f37c87de0645e99d10cb50e9233362a
Chris Westra <cwestra@gmail.com>
2013-05-28 23:44:21 -0400

    Add #add_readings to SensorReading

--- app/models/sensor_reading.rb
+++ app/models/sensor_reading.rb
@@ -4,4 +4,13 @@ class SensorReading
   belongs_to :sensor
   field :time, type: DateTime
   field :value
+
+  def self.add_readings(sensor_id, readings)
+    readings.reduce 0 do |added_count, reading|
+      r = SensorReading.new time: Time.parse(reading['time']), value: reading['value'].to_i
+      if r.save
+        added_count + 1
+      end
+    end
+  end
 end
