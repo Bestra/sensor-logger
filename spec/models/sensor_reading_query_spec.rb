724bffd14bb9e252670ea1ae63e043538e64ce78
Chris Westra <cwestra@gmail.com>
2013-05-30 09:37:16 -0400

    Add more specs for SensorReadingQuery

--- spec/models/sensor_reading_query_spec.rb
+++ spec/models/sensor_reading_query_spec.rb
@@ -1,20 +1,52 @@
+require 'spec_helper'
 describe SensorReadingQuery do
 
   describe "initializing" do
-
-
+    context "with a valid project" do
+      Given(:project) { FactoryGirl.create(:project) }
+      context "with existing sensors and data" do
+        Given(:sensor_1) { project.find_or_create_sensor(1) }
+        Given(:sensor_2) { project.find_or_create_sensor(2) }
+      end
+    end
   end
-
 
   describe "getting results" do
     context "with a valid project" do
       Given(:project) { FactoryGirl.create(:project) }
       context "with existing sensors and data" do
-      Given!(:sensors) do
-        project.find_or_create_sensor(1)
-        project.find_or_create_sensor(2)
-      end
+        Given(:sensor_1) { project.find_or_create_sensor(1) }
+        Given(:sensor_2) { project.find_or_create_sensor(2) }
+        Given!(:data) do
+          SensorReading.create(sensor_id: sensor_1.id, time: DateTime.now, value: 1)
+          SensorReading.create(sensor_id: sensor_2.id, time: DateTime.now - 1.year, value: 1)
+        end
 
+        context "getting all data" do
+          When(:result) { SensorReadingQuery.new(project).result }
+          Then { result[1][:readings].count.should == 1 }
+          Then { result[2][:readings].count.should == 1 }
+        end
+
+        context "getting data for one sensor" do
+          When(:result) { SensorReadingQuery.new(project, sensor_indices: [1] ).result }
+          Then { result[1][:readings].count.should == 1 }
+          Then { result[2].should be_nil }
+        end
+
+        context "data after a specified time" do
+          Given(:time) { (DateTime.now - 1.month).to_s }
+          When(:result) { SensorReadingQuery.new(project, start_time: time ).result }
+          Then { result[1][:readings].count.should == 1 }
+          Then { result[2][:readings].count.should == 0 }
+        end
+
+        context "data before a specified time" do
+          Given(:time) { (DateTime.now - 1.month).to_s }
+          When(:result) { SensorReadingQuery.new(project, end_time: time ).result }
+          Then { result[1][:readings].count.should == 0 }
+          Then { result[2][:readings].count.should == 1 }
+        end
       end
     end
   end
