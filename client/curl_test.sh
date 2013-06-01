7811ff2b5b654bda1146c8306a72266e75206e47
Chris Westra <cwestra@gmail.com>
2013-06-01 09:08:57 -0400

    Use project id for api stuff

--- client/curl_test.sh
+++ client/curl_test.sh
@@ -1,3 +1,3 @@
 curl -H "Authorization: Token token=zkzXDgydHF1RCyzZ8rK5" -H "Content-Type: application/json" \
-	-X POST -d '{"sensorReadings":{"project_id":"123","sensors":[{"project_index":"1","readings":[{"time":"12:00","value":"3"}]}]}}' \
+	-X POST -d '{"sensorReadings":{"project_id":"5192307bb4d3eddd7e000002","sensors":[{"project_index":"1","readings":[{"time":"12:03","value":"3"}]}]}}' \
 	http://localhost:3000/readings
